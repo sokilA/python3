7dcbbf9e5923897aaee900732286e204b164a503
sokilA <31885831+sokilA@users.noreply.github.com>
2018-09-24 10:43:03 +0300

    Add files via upload

---
+++ lab5_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+print('Enter height and width your door')
+
+height = int(input()) 
+width = int(input()) 
+
+print('Enter size of box')
+
+box_height = int(input()) 
+box_width = int(input()) 
+box_length = int(input()) 
+
+if height > box_height and width > box_width:
+	print('The box fit for that door')
+elif height > box_width and width > box_height:
+	print('The box fit for that door')
+elif height > box_height and width > box_length:
+	print('The box fit for that door')
+elif height > box_width and width > box_length:
+	print('The box fit for that door')
+else:
+	print('The box doesn`t fit for that door')
