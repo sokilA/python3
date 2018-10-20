2e8a9bc2036c02ae7fa5541813164a6be2305164
sokilA <31885831+sokilA@users.noreply.github.com>
2018-10-20 16:08:36 +0300

    Update lab7_3.py

--- lab7_3.py
+++ lab7_3.py
@@ -6,44 +6,48 @@ brackets = ['(',')', '[',']','{','}','<','>']
 
 def input_string() -> str:
 	''' Enter start string '''
-	entered_string = input('Enter your string: ')
-	return entered_string
+	starting_string = input('Enter your string: ')
+	return starting_string
 
 
-def check_couple_brackets(a:str) -> bool:
+def check_couple_brackets(starting_string:str) -> bool:
+	''' Check that in string there are even number of brackets '''
 	counter = 0
 	for x in brackets:
-		counter += a.count(x)
+		counter += starting_string.count(x)
 	if (counter % 2) == 0:
 		return False
 	else:
 		return True
 
 
-def remove_characters(a:str) -> str:
+def remove_characters(starting_string:str):
 	'''check couple of brackets and remove characters'''
-	if check_couple_brackets(a):
-		return 'false'
-
-	for x in a:
-		if x not in brackets:
-			new_string = a.replace(x,"")
-	return new_string
-
-
-def check_right_of_brackets(a:str) -> bool:
-	a = remove_characters(a)
-	if a == 'false':
+	if check_couple_brackets(starting_string):
 		return False
+
+	for x in starting_string:
+		if x not in brackets:
+			starting_string = starting_string.replace(x,"")
+	return starting_string
+
+
+def check_right_of_brackets(starting_string:str) -> bool:
+	'''check that brackets positioned correctly'''
+	if not remove_characters(starting_string):
+		return False
+		
+	string_without_letters = remove_characters(starting_string)
 	couple_brackets = ['()','{}','<>','[]']
-	while len(a) > 0:
-		startLen = len(a)
+
+	while len(string_without_letters) > 0:
+		startLen = len(string_without_letters)
 		for x in couple_brackets:
-			if x in a:
-				a = a.replace(x, "")
-			if len(a) == 0:
+			if x in string_without_letters:
+				string_without_letters = string_without_letters.replace(x, "")
+			if len(string_without_letters) == 0:
 				return True
-		if startLen == len(a):
+		if startLen == len(string_without_letters):
 			break
 	return False
 
