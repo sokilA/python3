c11179d292207a778a43877a5857c8385d020e23
sokilA <31885831+sokilA@users.noreply.github.com>
2018-10-20 19:17:21 +0300

    Update lab7_4.py

--- lab7_4.py
+++ lab7_4.py
@@ -9,11 +9,8 @@ def input_string() -> str:
 
 def encrypt(start_string:str) -> str:
 	''' encrypt your string '''
-	i = 0
-	while i < len(start_string):
-		for x in start_string:
-			start_string = start_string.replace(x,abc[abc.index(x)+1])
-			i += 1
+	for x in start_string:
+		start_string = start_string.replace(x,abc[abc.index(x)+1])
 	return start_string
 
 
